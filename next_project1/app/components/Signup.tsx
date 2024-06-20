dd401838853f00ce576aadd52c0fa532175d37fe
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-06-20 22:49:13 +0300

    axios response added to see if data rendering in the homepage

--- next_project1/app/components/Signup.tsx
+++ next_project1/app/components/Signup.tsx
@@ -2,10 +2,12 @@
 
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function Signup() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   return (
     <div className="h-screen flex justify-center flex-col">
@@ -35,10 +37,22 @@ export function Signup() {
                 placeholder="123456"
               />
               <button
+                onClick={async () => {
+                  const response = await axios.post(
+                    "http://localhost:3000/api/user",
+                    {
+                      username,
+                      password,
+                    }
+                  );
+                  if (response.status === 200) {
+                    router.push("/");
+                  }
+                }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
-                Sign in
+                Sign up
               </button>
             </div>
           </div>
